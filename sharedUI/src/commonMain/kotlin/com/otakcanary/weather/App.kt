aaa8f1b16968d34960fb677d105c99d52aec8adf
Ekaterina Levchenko <guinea.pig.prog@gmail.com>
2026-07-22 19:02:24 +0200

    Today temperature

--- sharedUI/src/commonMain/kotlin/com/otakcanary/weather/App.kt
+++ sharedUI/src/commonMain/kotlin/com/otakcanary/weather/App.kt
@@ -11,22 +11,19 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ContainedLoadingIndicator
+import androidx.compose.material3.LoadingIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.otakcanary.weather.domain.Day
 import com.otakcanary.weather.ui.WeatherViewModel
-import org.jetbrains.compose.resources.painterResource
-
-import weatherapp.sharedui.generated.resources.Res
-import weatherapp.sharedui.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
@@ -35,15 +32,30 @@ fun App() {
     val state by remember { viewModel.state }
     MaterialTheme {
         Scaffold(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.background)
         ) { paddingValues ->
             Column(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(paddingValues)
             ) {
-                if (state.isLoading) {
-                    Text("Loading...")
+                if (false) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        ContainedLoadingIndicator(modifier = Modifier.size(64.dp))
+                        Text(
+                            "Loading...",
+                            style = MaterialTheme.typography.displaySmall,
+                            color = MaterialTheme.colorScheme.onBackground,
+                            modifier = Modifier.padding(top = 16.dp)
+                        )
+                    }
                 } else {
+                    TodayWeather("Berlin", "2025", 25.5, 26.4, 18.0, Modifier)
                     LazyRow {
                         items(state.weather) { day: Day ->
                             WeatherDay(day, Modifier)
@@ -51,6 +63,38 @@ fun App() {
                     }
                 }
             }
+        }
+    }
+}
+
+
+@Composable
+fun TodayWeather(
+    city: String,
+    date: String,
+    temperature: Double,
+    highTemperature: Double,
+    lowTemperature: Double,
+    modifier: Modifier
+) {
+    Column() {
+        Text(
+            city,
+            style = MaterialTheme.typography.displayMedium
+        )
+        Text(
+            date,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                "$temperature°",
+                style = MaterialTheme.typography.displayLarge
+            )
+            Text(
+                "H:$highTemperature L:$lowTemperature",
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
         }
     }
 }
@@ -73,15 +117,15 @@ fun WeatherDay(
             .size(width = 64.dp, height = 107.dp) // TODO height should calculate automatically
             .clip(CircleShape)
 
-            .background(color = MaterialTheme.colorScheme.onSurface)
+            .background(color = MaterialTheme.colorScheme.secondaryContainer)
     ) {
         Text(
             text = day.date,
-            color = MaterialTheme.colorScheme.secondary
+            color = MaterialTheme.colorScheme.onSecondaryContainer
         )
         Text(
             text = day.temperature.toString(),
-            color = MaterialTheme.colorScheme.primary,
+            color = MaterialTheme.colorScheme.onSecondaryContainer,
             style = MaterialTheme.typography.titleLarge
         )
     }
@@ -89,6 +133,14 @@ fun WeatherDay(
 
 @Preview
 @Composable
-fun WeatherDayPreview() {
-    WeatherDay(Day("2025", 25.3), Modifier)
+fun TodayWeatherPreview() {
 }
+
+@Preview
+@Composable
+fun WeatherDayPreview() {
+    //WeatherDay(Day("2025", 25.3), Modifier)
+    TodayWeather("Berlin", "2025", 25.5, 26.4, 18.0, Modifier)
+
+}
+
--- sharedUI/src/commonMain/kotlin/com/otakcanary/weather/ui/theme/Theme.kt
+++ sharedUI/src/commonMain/kotlin/com/otakcanary/weather/ui/theme/Theme.kt
@@ -266,3 +266,4 @@ fun AppTheme(
         content = content
     )
 }
+
