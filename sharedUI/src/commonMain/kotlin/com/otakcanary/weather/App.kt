a35338f44161037f6de2220bd1dfd9335e70bb5f
Ekaterina Levchenko <guinea.pig.prog@gmail.com>
2026-07-22 20:06:21 +0200

    Finished UI draft

--- sharedUI/src/commonMain/kotlin/com/otakcanary/weather/App.kt
+++ sharedUI/src/commonMain/kotlin/com/otakcanary/weather/App.kt
@@ -1,18 +1,23 @@
 package com.otakcanary.weather
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.ContainedLoadingIndicator
-import androidx.compose.material3.LoadingIndicator
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -24,6 +29,9 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.otakcanary.weather.domain.Day
 import com.otakcanary.weather.ui.WeatherViewModel
+import org.jetbrains.compose.resources.painterResource
+import weatherapp.sharedui.generated.resources.Res
+import weatherapp.sharedui.generated.resources.temp_sun
 
 @Composable
 @Preview
@@ -56,11 +64,10 @@ fun App() {
                     }
                 } else {
                     TodayWeather("Berlin", "2025", 25.5, 26.4, 18.0, Modifier)
-                    LazyRow {
-                        items(state.weather) { day: Day ->
-                            WeatherDay(day, Modifier)
-                        }
-                    }
+                    Spacer(modifier = Modifier.height(8.dp))
+                    HourlyForecast(state.weather)
+                    Spacer(modifier = Modifier.height(8.dp))
+                    WeekForecast(state.weather)
                 }
             }
         }
@@ -77,7 +84,7 @@ fun TodayWeather(
     lowTemperature: Double,
     modifier: Modifier
 ) {
-    Column() {
+    Column(modifier = modifier.fillMaxWidth()) {
         Text(
             city,
             style = MaterialTheme.typography.displayMedium
@@ -86,7 +93,11 @@ fun TodayWeather(
             date,
             color = MaterialTheme.colorScheme.onSurfaceVariant
         )
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Image(painterResource(Res.drawable.temp_sun), contentDescription = null)
             Text(
                 "$temperature°",
                 style = MaterialTheme.typography.displayLarge
@@ -100,7 +111,70 @@ fun TodayWeather(
 }
 
 @Composable
-fun WeatherDay(
+fun HourlyForecast(days: List<Day>) {
+    Text(
+        "Hourly",
+        style = MaterialTheme.typography.headlineLarge
+    )
+    LazyRow(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+        items(days) { day: Day ->
+            WeatherHour(day, Modifier)
+        }
+    }
+}
+
+
+@Composable
+fun WeekForecast(days: List<Day>) {
+    Text(
+        "This Week",
+        style = MaterialTheme.typography.headlineLarge
+    )
+    LazyColumn {
+        items(days) { day: Day ->
+            WeatherDay(day, Modifier)
+            HorizontalDivider(Modifier)
+        }
+    }
+}
+
+@Composable
+fun WeatherDay(day: Day, modifier: Modifier) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text("Today")
+        WeatherDescription()
+        HighAndLowTemperatures(day.temperature, day.temperature, modifier)
+    }
+}
+
+@Composable
+fun HighAndLowTemperatures(high: Double, low: Double, modifier: Modifier) {
+    Row {
+        Text("${high}°")
+        Text(
+            "${low}°",
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+    }
+}
+
+@Composable
+fun WeatherDescription() {
+    Row {
+        Image(
+            painterResource(Res.drawable.temp_sun), contentDescription = null,
+            modifier = Modifier.height(32.dp)
+        )
+        Text("Golden and Bright")
+    }
+}
+
+
+@Composable
+fun WeatherHour(
     day: Day,
     modifier: Modifier
 ) {
@@ -109,8 +183,6 @@ fun WeatherDay(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .padding(
-                start = 8.dp,
-                end = 8.dp,
                 top = 12.dp,
                 bottom = 12.dp
             ) // TODO they must be distributed with space between them
@@ -133,14 +205,20 @@ fun WeatherDay(
 
 @Preview
 @Composable
+fun WeatherHourPreview() {
+    WeatherHour(Day("2025", 25.5), Modifier)
+}
+
+@Preview
+@Composable
 fun TodayWeatherPreview() {
+    TodayWeather("Berlin", "2025", 25.5, 26.4, 18.0, Modifier)
 }
 
 @Preview
 @Composable
 fun WeatherDayPreview() {
-    //WeatherDay(Day("2025", 25.3), Modifier)
-    TodayWeather("Berlin", "2025", 25.5, 26.4, 18.0, Modifier)
-
+    WeatherDay(Day("2025", 25.5), Modifier)
 }
+
 
